c6390d8e288998c68c4c91aa4cec6141102e2d5f
Manraaj Nijjar <ManraajNijjar@gmail.com>
2018-01-21 16:51:36 -0500

    Setup: Initital files added

---
+++ TextFieldWithButton/TextFieldWithButton.swift
@@ -0,0 +1,21 @@
+//
+//  TextFieldWithButton.swift
+//  TextFieldWithButton
+//
+//  Created by Manraaj Nijjar on 1/21/18.
+//  Copyright © 2018 Manraaj Nijjar. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldWithButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
